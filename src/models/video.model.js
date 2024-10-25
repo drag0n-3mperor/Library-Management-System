ecf7aa47276b27f9db2ef4e98195ba3f138b925f
Bitan Das <biggyansagar@gmail.com>
2024-10-26 03:09:01 +0530

    added user and video models

---
+++ src/models/video.model.js
@@ -0,0 +1,44 @@
+import {Schema, model} from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const videoSchema = new Schema(
+    {
+        videoFile: {
+            type: String,
+            required: true,
+        },
+        thumnail: {
+            type: String,
+            required: true,
+        },
+        title: {
+            type: String,
+            required: true,
+        },
+        description: {
+            type: String,
+            required: true,
+        },
+        duration: {
+            type: Number,
+            required: true,
+        },
+        views: {
+            type: Number,
+            default: 0,
+        },
+        isPublished: {
+            type: Boolean,
+            default: false,
+        },
+        owner: {
+            type: Schema.Types.ObjectId,
+            ref: "User",
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+export const Video = model("Video", videoSchema);
