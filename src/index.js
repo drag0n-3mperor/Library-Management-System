a735559740a500b579a8a2473bbf2c54d2cfce13
Bitan Das <biggyansagar@gmail.com>
2024-10-26 00:12:53 +0530

    added ApiResponse and ApiError files

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import dotenv from "dotenv";
 dotenv.config();
+import app from "./app.js";
+
 import connDB from "./db/conn.js";
 
-connDB();
+connDB().
+then(() => {
+    app.listen(process.env.PORT, () => {
+        console.log(`Server connection sucessful at port ${process.env.PORT}`);
+    })
+    app.on("error", err => console.error(err));
+});
